23ad974c97ba4241631f0a5b8e6e4ab4e97b2e9c
Evan M. Bonk <embonk@hotmail.com>
2025-09-02 12:14:20 -0600

    item view created and connected

--- frontend/src/pages/ItemView.jsx
+++ frontend/src/pages/ItemView.jsx
@@ -1,13 +1,47 @@
 import { useState, useEffect } from "react";
-
+import { getAllItems } from "../api/api.js";
 
 function ItemView() {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const temp = async () => {
+      let data = await getAllItems();
+      console.log(data);
+      setItems(data);
+    }
+    temp();
+
+  }, []);
 
 
   return (
     <div className="itemview">
       <h1>Item View</h1>
+
+      <button>My Items</button>
+
+      <div className="itemTable">
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Owner</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Quantity</th>
+          </tr>
+          {items.map((item, key) => {
+            return (<tr>
+              <td>{item.item_id}</td>
+              <td>{item.user}</td>
+              <td>{item.item_name}</td>
+              <td>{item.description}</td>
+              <td>{item.quantity}</td>
+            </tr>)
+          })}
+        </table>
+
+      </div>
     </div>
   )
 }
