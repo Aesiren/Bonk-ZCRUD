e4229296ef6377c73ec712a6be79b87b090c6d04
Evan M. Bonk <embonk@hotmail.com>
2025-09-02 16:22:46 -0600

    item view complete

--- frontend/src/pages/ViewItem.jsx
+++ frontend/src/pages/ViewItem.jsx
@@ -9,34 +9,45 @@ function ViewItem() {
 
   useEffect(() => {
     const getItem = async () => {
-      let temp = await getOneItem(itemId);
-      setItem(temp);
+      try {
+        let temp = await getOneItem(itemId);
+        setItem(temp);
+      } catch (err) {
+        console.error("Failed to fetch data")
+      }
+
     }
     getItem();
+  }, [itemId])
 
-    console.log(itemId);
-    console.log(item);
-  }, [])
+  useEffect(() => {
+    if (item) {
+      console.log("Item updated ", item, item[0].item_id);
+    }
+  }, [item])
 
+  if (!item) {
+    return (<>Loading</>)
+  }
 
   return (
     <div className="view-item">
       <h1>Item Details</h1>
 
       <label>Item ID:</label>
-      <span>{item.item_id}</span>
-
+      <label>{item[0].item_id}</label>
+      <br />
       <label>Owning User:</label>
-      <span>{item.user}</span>
-
+      <label>{item[0].user}</label>
+      <br />
       <label>Item Name:</label>
-      <span>{item.item_name}</span>
-
+      <label>{item[0].item_name}</label>
+      <br />
       <label>Description:</label>
-      <span>{item.description}</span>
-
+      <label>{item[0].description}</label>
+      <br />
       <label>Quantity:</label>
-      <span>{item.quantity}</span>
+      <label>{item[0].quantity}</label>
 
     </div>
   )
